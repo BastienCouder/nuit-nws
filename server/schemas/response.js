dc6c1a9fcd0eb019d58e2becf1e9dca894d2ebb1
Bastien <bastien.couder@gmail.com>
2024-01-29 10:17:37 +0100

    final schemas

---
+++ server/schemas/response.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+// Création du schéma
+const responseSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  idQuestion: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Answer",
+    required: true,
+  },
+  idUtilisateur: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  contenu: { type: String, required: true },
+  dateReponse: { type: Date, default: Date.now },
+});
+
+// Création du modèle
+export default mongoose.model("Response", responseSchema);
