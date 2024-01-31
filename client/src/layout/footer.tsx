6f51db3878afc87e9b553897bca75646fc398bb8
2bleG <2bleg.akagucci@gmail.com>
2024-01-31 12:18:16 +0100

    changement icone

--- client/src/layout/footer.tsx
+++ client/src/layout/footer.tsx
@@ -5,13 +5,13 @@ export default function Footer() {
     <footer className="text-background bg-background pt-4 pl-10 pr-10 fixed bottom-0 w-full">
       <ul className="list-none flex justify-around bg-primary p-4 rounded-tl-[50px] rounded-tr-[50px] bottom-0 mt-auto">
         <li>
-          <BarChart2 strokeWidth={3} size={32} />
-        </li>
-        <li>
           <UserRound strokeWidth={3} size={32} />
         </li>
         <li>
           <Camera strokeWidth={3} size={32} />
+        </li>
+        <li>
+          <BarChart2 strokeWidth={3} size={32} />
         </li>
       </ul>
     </footer>
