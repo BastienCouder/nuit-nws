741999d0489555c1dd4d6aebe19a9e710240840f
2bleG <2bleg.akagucci@gmail.com>
2024-01-31 11:38:21 +0100

    les ptites icones

--- client/src/layout/footer.tsx
+++ client/src/layout/footer.tsx
@@ -1,7 +1,13 @@
+import { BarChart2, Camera, UserRound } from "lucide-react";
+
 export default function Footer() {
   return (
     <>
-      <footer>footer</footer>
+      <footer className="text-primary bg-background p-4 text-center">
+      <BarChart2 strokeWidth={3} size={24}/>
+      <UserRound strokeWidth={3} size={24} />
+      <Camera strokeWidth={3} size={24} />
+      </footer>
     </>
   );
 }
--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const dbUri = process.env.MONGO_URI;
+    const dbUri = "mongodb+srv://nuit-nws:tk9bidUMFFKtyaY9@nuit-nws.lujkzjq.mongodb.net/";
     if (!dbUri) {
       throw new Error("MONGO_URI is not defined in .env");
     }
