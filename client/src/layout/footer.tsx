e6e10cca0088d033dd2c781d44483e7c8af99c40
2bleG <2bleg.akagucci@gmail.com>
2024-01-31 12:14:23 +0100

    footer

--- client/src/layout/footer.tsx
+++ client/src/layout/footer.tsx
@@ -2,12 +2,18 @@ import { BarChart2, Camera, UserRound } from "lucide-react";
 
 export default function Footer() {
   return (
-    <>
-      <footer className="text-primary bg-background p-4 text-center">
-      <BarChart2 strokeWidth={3} size={24}/>
-      <UserRound strokeWidth={3} size={24} />
-      <Camera strokeWidth={3} size={24} />
-      </footer>
-    </>
+    <footer className="text-background bg-background pt-4 pl-10 pr-10 fixed bottom-0 w-full">
+      <ul className="list-none flex justify-around bg-primary p-4 rounded-tl-[50px] rounded-tr-[50px] bottom-0 mt-auto">
+        <li>
+          <BarChart2 strokeWidth={3} size={32} />
+        </li>
+        <li>
+          <UserRound strokeWidth={3} size={32} />
+        </li>
+        <li>
+          <Camera strokeWidth={3} size={32} />
+        </li>
+      </ul>
+    </footer>
   );
 }
